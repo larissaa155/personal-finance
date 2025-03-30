7613219cc313fa4358a8f526857592bf52d723d8
Larissa Aziz <135729951+larissaa155@users.noreply.github.com>
2025-03-29 22:26:34 -0400

    Delete lib directory

--- lib/screens/DashboardScreen.dart
+++
@@ -1,7 +0,0 @@
-import 'package:your_app/widgets/SmartInsightsCard.dart'; 
-import 'package:your_app/services/ai_insights_service.dart'; 
-
-SmartInsightsCard(
-  transactions = yourTransactionList,
-  currentBalance = 500.00,
-),
